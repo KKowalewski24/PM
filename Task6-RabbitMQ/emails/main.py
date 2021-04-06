8b5bb20535bb49242154291ee342525d0aabd017
Kamil Kowalewski <szamil24@gmail.com>
2021-04-06 20:40:22 +0200

    Add more

--- Task6-RabbitMQ/emails/main.py
+++ Task6-RabbitMQ/emails/main.py
@@ -1,11 +1,31 @@
+import os
+
+import pika
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=os.environ["BROKER_HOST"]))
+channel = connection.channel()
+channel.queue_declare(queue='task_queue', durable=True)
 
-@app.route("/")
+
+def send_email(ch, method, properties, body) -> None:
+    email = body.decode()
+    print("Email has been sent!")
+    print("Email address: " + str(email))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=send_email)
+channel.start_consuming()
+
+
+@app.route("/", methods=["GET"])
 def home() -> str:
-    return jsonify("Emails working")
+    return jsonify(info="Emails working")
 
 
 def main() -> None:
--- Task6-RabbitMQ/users/main.py
+++ Task6-RabbitMQ/users/main.py
@@ -1,11 +1,29 @@
+import os
+
+import pika
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home() -> str:
-    return jsonify("Users working")
+    return jsonify(info="Users working")
+
+
+# def create_user() -> str:
+#     message = "sample_email"
+#
+#     connection = pika.BlockingConnection(
+#         pika.ConnectionParameters(host=os.environ["BROKER_HOST"]))
+#     channel = connection.channel()
+#     channel.queue_declare(queue='task_queue', durable=True)
+#     channel.basic_publish(
+#         exchange='',
+#         routing_key='task_queue',
+#         body=message,
+#         properties=pika.BasicProperties(delivery_mode=2))
+#     connection.close()
 
 
 def main() -> None:
